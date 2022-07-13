9cafba0960c59825ea8caa9a91ed2e78f65b8098
tamim-el <58140560+tamim-el@users.noreply.github.com>
2022-07-13 15:06:57 +0200

    Add files via upload

--- Utils/load_data.py
+++ Utils/load_data.py
@@ -1,8 +1,15 @@
 import numpy as np
+import csv
+import h5py
 from skmultilearn.dataset import load_from_arff
+import scipy.stats as st
 from scipy.stats import norm
+from scipy.io import loadmat
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelBinarizer
+import os
+import glob
 
 
 def toy_dataset_1_perturbed(n, d):
@@ -22,13 +29,41 @@ def toy_dataset_1_perturbed(n, d):
     return X, Y_true, Y_obs
 
 
+def toy_quantile(n=50, t_min=0., t_max=1.5, noise=1., probs=[0.5]):
+    """
+    Parameters
+    n: number of points (t, y)
+    t_min: minimum input data t
+    t_max: maximum input data t
+    noise: noise level
+    probs: probabilities (quantiles levels)
+
+    Returns:
+    x: sorted random data in [t_min, t_max]
+    y: targets corresponding to x (following a noisy sin curve)
+    q: true quantiles corresponding to x
+    """
+    t_down, t_up = 0., 1.5  # Bounds for the noise
+    t = np.random.rand(n) * (t_max-t_min) + t_min
+    t = np.sort(t)
+    pattern = -np.sin(2*np.pi*t)  # Pattern of the signal
+    enveloppe = 1 + np.sin(2*np.pi*t/3)  # Enveloppe of the signal
+    pattern = pattern * enveloppe
+    # Noise decreasing std (from noise+0.2 to 0.2)
+    noise_std = 0.2 + noise*(t_up - t) / (t_up - t_down)
+    # Gaussian noise with decreasing std
+    add_noise = noise_std * np.random.randn(n)
+    observations = pattern + add_noise
+    quantiles = [pattern + norm.ppf(p, loc=np.zeros(n),
+                                    scale=np.fabs(noise_std)) for p in probs]
+    return t, observations, quantiles
+
+
 def load_otoliths(filename_prefix_tr='Data/otoliths/train/Train_fc_',
                   training_info_path_tr='Data/otoliths/train/Training_info.csv',
                   filename_prefix_te='Data/otoliths/test/Test_fc_',
                   training_info_path_te='Data/otoliths/test/Testing_info.csv'):
-    """
-        Load Dataset otoliths
-    """
+
     filename_suffix = '.npy'
     n = 3780
     X_train = np.zeros((n, 64 * 64))
@@ -49,6 +84,7 @@ def load_otoliths(filename_prefix_tr='Data/otoliths/train/Train_fc_',
 
 
 def load_rf1(path_tr='Data/rf1/rf1-train.arff', path_te='Data/rf1/rf1-test.arff', normalize=True):
+
     """
         Load Dataset rf1
     """
@@ -68,9 +104,11 @@ def load_rf1(path_tr='Data/rf1/rf1-train.arff', path_te='Data/rf1/rf1-test.arff'
 
 
 def load_rf2(path_tr='Data/rf2/rf2-train.arff', path_te='Data/rf2/rf2-test.arff', normalize=True):
+
     """
         Load Dataset rf2
     """
+
     x, y = load_from_arff(path_tr, label_count=8)
     X_tr, Y_tr = x.todense(), y.todense()
 
@@ -87,9 +125,11 @@ def load_rf2(path_tr='Data/rf2/rf2-train.arff', path_te='Data/rf2/rf2-test.arff'
 
 
 def load_scm1d(path_tr='Data/scm1d/scm1d-train.arff', path_te='Data/scm1d/scm1d-test.arff', normalize=True):
+
     """
         Load Dataset scm1d
     """
+
     x, y = load_from_arff(path_tr, label_count=16)
     X_tr, Y_tr = x.todense(), y.todense()
 
@@ -106,9 +146,11 @@ def load_scm1d(path_tr='Data/scm1d/scm1d-train.arff', path_te='Data/scm1d/scm1d-
 
 
 def load_scm20d(path_tr='Data/scm20d/scm20d-train.arff', path_te='Data/scm20d/scm20d-test.arff', normalize=True):
+
     """
         Load Dataset scm20d
     """
+
     x, y = load_from_arff(path_tr, label_count=16)
     X_tr, Y_tr = x.todense(), y.todense()
 
@@ -122,3 +164,35 @@ def load_scm20d(path_tr='Data/scm20d/scm20d-train.arff', path_te='Data/scm20d/sc
         X_te = scaler.transform(X_te)
 
     return X_tr, Y_tr, X_te, Y_te
+
+
+def load_sarcos(N=50, path_tr='Data/sarcos/sarcos_inv.mat', path_te='Data/sarcos/sarcos_inv_test.mat', normalize=True):
+
+    """
+        Load Dataset Sarcos
+    """
+
+    data_tr = loadmat(path_tr)
+    x, y = data_tr['sarcos_inv'][:, :21].copy(), data_tr['sarcos_inv'][:, 21:].copy()
+    idx = np.random.choice(x.shape[0], N * 7, replace=False)
+    X_tr, Y_tr_full = x[idx].copy(), y[idx].copy()
+    idx_t = np.empty(0)
+    for i in range(7):
+        idx_t = np.concatenate((idx_t, i * np.ones(N)))
+    idx_t = np.random.permutation(idx_t)
+    idx_t = idx_t.astype(int)
+    Y_tr = np.choose(idx_t, Y_tr_full.T)
+    lb = LabelBinarizer()
+    lb.fit([1, 2, 3, 4, 5, 6, 7])
+    T_tr = lb.transform(idx_t + 1)
+
+    data_te = loadmat(path_te)
+    X_te, Y_te = data_te['sarcos_inv_test'][:, :21].copy(), data_te['sarcos_inv_test'][:, 21:].copy()
+
+    # Normalizing
+    if normalize:
+        scaler = StandardScaler()
+        X_tr = scaler.fit_transform(X_tr)
+        X_te = scaler.transform(X_te)
+
+    return X_tr, T_tr, Y_tr, Y_tr_full, X_te, Y_te
